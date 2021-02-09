a9e5f61bbd8baff6b5c19d895b6e26e98dd15999
Ahmed Mire <afmire877@gmail.com>
2021-02-09 01:20:32 +0000

    adding relationships between tags and meals

--- app/Models/Meal.php
+++ app/Models/Meal.php
@@ -6,7 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Meal extends Model
 {
+    protected $casts = [
+        "quantity" => "integer",
+        "collection_point_id" => "integer"
+
+    ];
     public function collectionPoint() {
         return $this->belongsTo(CollectionPoint::class );
+    }
+    public function tags() {
+        return $this->belongsToMany(Tag::class);
     }
 }
