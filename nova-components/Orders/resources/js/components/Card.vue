0645f50577134a76b2899e7eb5fce8f4287abb99
Ahmed Mire <afmire877@gmail.com>
2021-04-09 18:31:57 +0100

    added Todays orders component

---
+++ nova-components/Orders/resources/js/components/Card.vue
@@ -0,0 +1,30 @@
+<template>
+    <card class="flex flex-col items-center justify-center cursor-pointer"
+          @click="redirect" >
+        <div class="px-3 py-3">
+            <h1 class="font-bold text-center text-xl text-80 font-light mb-5">Total Orders</h1>
+            <br>
+            <p class="text-center text-4xl">{{ card.orders.length }}</p>
+        </div>
+    </card>
+</template>
+
+<script>
+export default {
+    props: [
+        'card',
+        // The following props are only available on resource detail cards...
+        // 'resource',
+        // 'resourceId',
+        // 'resourceName',
+    ],
+    methods: {
+       redirect: function() {
+            console.log("aksjdlajs")
+            // window.location.href = window.location.origin + '/nova/resources/orders'
+       }
+    },
+    mounted() {
+    },
+}
+</script>
