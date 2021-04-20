5d0eb42222e53777dd757684fdbe2e0e0bbaff1b
Ahmed Mire <afmire877@gmail.com>
2021-04-20 23:21:28 +0100

    Allowed charity to create meals

--- app/Policies/MealPolicy.php
+++ app/Policies/MealPolicy.php
@@ -22,19 +22,19 @@ class MealPolicy
 
     public function viewAny(User $user)
     {
-        return  $user->type == 'charity' || "admin";
+        return  $user->type == "charity" || "admin";
     }
     public function view(User $user)
     {
-        return  $user->type == 'charity' || "admin";
+        return  $user->type == "charity" || "admin";
     }
     public function create(User $user)
     {
-        return  $user->type == "admin";
+        return  $user->type == "admin" || "charity" ;
     }
     public function update(User $user, Meal $meal)
     {
-        return  $user->type == 'charity' || "admin";
+        return  $user->type == "charity" || "admin";
     }
     public function delete(User $user, Meal $meal)
     {
